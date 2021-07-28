06c5dc36b1f92f59c1f89e316a3497de7c60a04f
yaelbu <yael.buisine@gmail.com>
2021-07-28 22:40:29 +0300

    Add project files.

---
+++ Organisation/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Organisation.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Firstnane { get; set; }
+        public string Lastname { get; set; }
+        public string Status { get; set; }
+        public string Position { get; set; }
+        public int ManagerId { get; set; }
+        public string Image { get; set; }
+    }
+}
